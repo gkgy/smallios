88d51d3503805fbc80c564f8daa12bb17a3b68f1
gkgy（天使的龙蛋） <gkgy@qq.com>
2017-04-15 16:22:14 +0800

    第一个samllword

---
+++ changelabel/changelabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  changelabel
+//
+//  Created by 高坤 on 2017/4/15.
+//  Copyright © 2017年 gkgy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
